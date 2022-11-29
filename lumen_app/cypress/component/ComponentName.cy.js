b04b2ff3ff7b133d161de7301b6dabd00ecf26d7
dannybains2 <dannybains2@gmail.com>
2022-11-29 16:48:26 +0000

    cypress tests complete and react tests started

--- lumen_app/cypress/component/ComponentName.cy.js
+++ lumen_app/cypress/component/ComponentName.cy.js
@@ -2,7 +2,7 @@ describe('CRUD-Operation-payload', () => {
   it('get', () => {
     cy.request({
         method : 'GET',
-        url : 'http://localhost:3001/api/resources',
+        url : 'http://localhost:3001/api/v2/resources',
   }).then(function(response){
     expect(response.body).have.property('payload');
   })
@@ -14,7 +14,7 @@ describe('CRUD-Operation-success', () => {
   it('get', () => {
     cy.request({
         method : 'GET',
-        url : 'http://localhost:3001/api/resources',
+        url : 'http://localhost:3001/api/v2/resources',
   }).then(function(response){
     expect(response.body).have.property('success');
   })
@@ -22,13 +22,35 @@ describe('CRUD-Operation-success', () => {
 })
 
 
-describe('CRUD-Operation-post', () => {
+describe('CRUD-Operation-get', () => {
   it('get', () => {
     cy.request({
         method : 'GET',
-        url : 'http://localhost:3001/api/resources',
+        url : 'http://localhost:3001/api/v2/resources',
   }).then(function(response){
     expect(response.body.payload[0]).have.property('title');
+  })
+  })
+})
+
+
+describe('CRUD-Operation-post', () => {
+  it('POST', () => {
+    cy.request({
+        method : 'POST',
+        url : 'http://localhost:3001/api/v2/resources',
+        headers: {
+          "Content-Type": "application/json;charset=UTF-8",
+        },
+        body : {
+          "title" : "Excalidraw",
+          "url" : "https://excalidraw.com/",
+          "language" : "n/a",
+          "submission_notes" : "This really easy to use",
+          "category_id" : "7"
+        }
+  }).then(function(response){
+    expect(response.body).have.property('payload');
   })
   })
 })
