d6d56a2825105fa91597c5a88775f5dfe50e12e7
november-golf <n.s.gray@live.co.uk>
2022-11-29 13:35:04 +0000

    JSdocs comments added to SearchBar.

--- lumen_app/src/components/SearchBar/index.js
+++ lumen_app/src/components/SearchBar/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 
+
+/**
+ * 
+ * @param {string} value - Is a string generated as the user types into the input field.
+ * @param {string} className - A string for the class name. 
+ * @param {func} handleChange - A function that sets the SearchValue state to the e.target.value of the input field as the user types to search.
+ * compares the user input with the Resources state (set initially to contain all resources from database fetch request (see AddAndSearchDisplay Ln - 17)) returning any results in the Resources state that contain a partial match.
+ * 
+ * @param {string} placeholder - A string that sets the text displayed in the input before any the user types to search..
+ * 
+ * @returns A search bar that calls a handleChange function as the user types to search.
+ */
 function SearchBar({ value, className, handleChange, placeholder }) {
   return (
     <div className="search-bar-container">
